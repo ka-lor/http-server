158e99053ba2d15dcd841a85daae07cbae4d77b3
Ka Lor <ka.lor329@gmail.com>
2018-05-13 22:20:29 -0700

    add console log to app.listen()

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('listening on port 3001...')
+});
